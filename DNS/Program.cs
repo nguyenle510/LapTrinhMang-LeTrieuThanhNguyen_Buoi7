d92e16896a8ea8823236f4cb93c12693db8d7a63
Nguyen Le <trieunguyen30@gmail.com>
2017-11-21 08:05:51 +0700

    update code

--- DNS/Program.cs
+++ DNS/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace DNS
 {
@@ -10,6 +11,20 @@ namespace DNS
     {
         static void Main(string[] args)
         {
+            Console.Write("Nhap ten mien hoac ip: ");
+            string host = Console.ReadLine();
+
+            IPHostEntry e = Dns.GetHostEntry(host);
+            Console.WriteLine("Hostname: {0}", e.HostName);
+
+            // In tên miền hoặc bí danh
+            foreach (string s in e.Aliases)
+                Console.WriteLine("\t+ Alias: {0} \n", s);
+
+            // Danh sách địa chỉ IP
+            foreach (IPAddress i in e.AddressList)
+                Console.WriteLine("\t+ IP: {0}", i);
+            Console.WriteLine();
         }
     }
 }
